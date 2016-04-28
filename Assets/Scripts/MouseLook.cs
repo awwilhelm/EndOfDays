8861693135317b674fdae42419926f2dac80b561
Brian Fitzgerald <cooldude242@gmail.com>
2016-04-27 19:19:20 -0500

    did some stuff

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -34,6 +34,7 @@ public class MouseLook : MonoBehaviour
     {
         // Ensure the cursor is always locked when set
         Screen.lockCursor = lockCursor;
+        Cursor.lockState = CursorLockMode.Locked;
  
         // Allow the script to clamp based on a desired target value.
         var targetOrientation = Quaternion.Euler(targetDirection);
